533106a032429784eac2ef1be5dce55b273903da
Sunday <sunday@Sundays-MacBook-Pro-2.local>
2024-09-06 01:04:47 +0100

    Added validation script to pages and limit

--- src/feature/core/dto/pagination.request.dto.ts
+++ src/feature/core/dto/pagination.request.dto.ts
@@ -1,14 +1,19 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, Max, Min } from "class-validator";
+import { Transform } from "class-transformer";
+import { IsInt, IsNotEmpty, Max, Min } from "class-validator";
 
 export class PaginationRequestDto {
   @IsNotEmpty()
   @ApiProperty()
+  @IsInt()
+  @Transform(({ value }) => parseInt(value, 10))
   @Min(1)
   page: number = 1
 
   @IsNotEmpty()
   @ApiProperty()
+  @IsInt()
+  @Transform(({ value }) => parseInt(value, 10))
   @Min(10)
   @Max(50)
   limit: number = 10
