71c2f42725ae8971de03b4c0527ceb774c1bd093
Sunday <sunday@Sundays-MacBook-Pro-2.local>
2024-09-06 01:17:21 +0100

    Added validation script to pages and limit

--- src/feature/community/community.repository.ts
+++ src/feature/community/community.repository.ts
@@ -420,7 +420,7 @@ export class CommunityRepository {
    */
   async searchCommunity(user: string, query: string, page: number, limit: number): Promise<PaginatedResult<any>> {
     return await this.paginator.paginate(this.communityModel,
-      { status: ACCOUNT_STATUS.APPROVED, account: new Types.ObjectId(user), $text: { $search: query } },
+      { status: ACCOUNT_STATUS.APPROVED, account: { $ne: new Types.ObjectId(user) }, $text: { $search: query } },
       {
         select: '_id name code description address members image type createdAt updatedAt',
         limit: limit,
