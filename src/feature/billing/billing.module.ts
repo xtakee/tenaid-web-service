bc7aceb89dd672a1baf250807e7329453e586f52
Sunday <sunday.okpoluaefe@gmail.com>
2025-02-11 10:44:23 +0100

    Added billing module

--- src/feature/billing/billing.module.ts
+++ src/feature/billing/billing.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { BillingController } from './billing.controller';
 import { BillingService } from './billing.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Billable, BillableSchema } from './model/billable';
+import { Billing, BillingSchema } from './model/billing';
 
 @Module({
   controllers: [BillingController],
-  providers: [BillingService]
+  providers: [BillingService],
+  imports: [
+    MongooseModule.forFeature([{ name: Billable.name, schema: BillableSchema }]),
+    MongooseModule.forFeature([{ name: Billing.name, schema: BillingSchema }]),
+  ]
 })
 export class BillingModule {}
