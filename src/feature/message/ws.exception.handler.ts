2751316bec90487324d6d86bc40e49fc1f82373e
Sunday <sunday.okpoluaefe@gmail.com>
2025-03-27 07:32:08 +0100

    Fix issue with community gates

--- src/feature/message/ws.exception.handler.ts
+++ src/feature/message/ws.exception.handler.ts
@@ -10,6 +10,7 @@ export class WsExceptionHandler implements WsExceptionFilter {
   catch(exception: BadRequestException | WsException, host: ArgumentsHost) {
     const ctx = host.switchToWs()
     const client: Socket = ctx.getClient()
+    const event = ctx.getData().event
 
     if (!client) return
 
@@ -23,7 +24,7 @@ export class WsExceptionHandler implements WsExceptionFilter {
       message = exception.getError() as string
     }
 
-    client.emit(WS_MESSAGE_ERROR, message)
+    client.emit(WS_MESSAGE_ERROR, { message, event })
     return
   }
 }
